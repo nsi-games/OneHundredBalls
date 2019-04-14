5c2644b9d915a78c36f35d1337c187b438aeed29
Manny Vaccaro <emmanuel.vaccaro@gmail.com>
2019-04-14 11:15:16 +1000

    Initial commit

---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OneHundredBalls
+{
+    public class Bucket : MonoBehaviour
+    {
+        public float speed = 5f;
+        public Vector2 direction = Vector2.right;
+
+        void Update()
+        {
+            transform.position += (Vector3)direction * speed * Time.deltaTime;    
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.name.Contains("Ball"))
+            {
+                other.transform.SetParent(transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OneHundredBalls
+{
+    public class Paddle : MonoBehaviour
+    {
+        private Animator anim;
+
+        // Use this for initialization
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                anim.SetBool("IsOpened", true);
+            }else
+            {
+                anim.SetBool("IsOpened", false);
+            }
+        }
+    }
+}
